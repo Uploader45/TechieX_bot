7ef860d8e37ffd306fdf9d524527721ce9fe7895
Uploader45 <96682675+Uploader45@users.noreply.github.com>
2022-08-14 11:42:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5389849727:AAHUcsA7UtQddWj_vQat_jnsdZfv5dJ-tso")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = int(os.environ.get("API_ID", "15668875"))
+    API_HASH = os.environ.get("API_HASH", "9126d3e935a1553bedf9bd822f3b4ad9")
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -24,8 +24,8 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    SESSION_NAME = "TECHIEX-BOT"
+    OWNER_ID = int(os.environ.get("OWNER_ID", "579974245"))
+    SESSION_NAME = "KNIGHT-RANGER-BOT"
     # database uri (mongodb)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
     MAX_RESULTS = "50"
